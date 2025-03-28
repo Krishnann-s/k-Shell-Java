45399769f38d483e91a96dde28bd5c5d1d175dc7
Krishnan <krishnan.spu@gmail.com>
2025-03-29 02:19:57 +0530

    feat: Run a program #ip1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -13,46 +15,63 @@ public class Main {
             System.out.print("$ "); // Shell starts with dollar
             String input = scanner.nextLine(); // accept any input
             String[] str = input.split(" ");
-            String command = str[0];
-            String parameter = "";
 
-            if (str.length > 2) {
-                for (int i = 1; i < str.length; i++) {
-                    if (i < str.length - 1) {
-                        parameter += str[i] + " ";
-                    } else {
-                        parameter += str[i];
-                    }
-                }
-            } else if (str.length > 1) {
-                parameter = str[1];
-            }
+            if (str.length == 0) continue;
+
+            String command = str[0];
+            String[] parameters = new String[str.length - 1];
+            System.arraycopy(str, 1, parameters, 0, str.length - 1);
 
             switch (command) {
                 case "exit" -> {
-                    if (parameter.equals("0")) {
+                    if (parameters.length == 1 && parameters[0].equals("0")) {
                         System.exit(0);
                     } else {
                         System.out.println(input + ": command not found");
                     }
                 }
-                case "echo" -> System.out.println(parameter);
+                case "echo" -> System.out.println(String.join(" " + parameters));
                 case "type" -> {
-                    if (parameter.equals(builtins.get(0)) ||
-                            parameter.equals(builtins.get(1)) ||
-                            parameter.equals(builtins.get(2))) {
-                        System.out.println(parameter + " is a shell builtin");
+                    if (builtins.contains(parameters[0])) {
+                        System.out.println(parameters[0] + " is a shell builtin");
                     } else {
-                        String path = getPath(parameter);
+                        String path = getPath(parameters[0]);
                         if (path != null) {
-                            System.out.println(parameter + " is " + path);
+                            System.out.println(parameters[0] + " is " + path);
                         } else {
-                            System.out.println(parameter + ": not found");
+                            System.out.println(parameters[0] + ": not found");
                         }
                     }
                 }
-                default -> System.out.println(input + ": command not found");
+                default -> runExternalCommand(command, parameters);
             }
+        }
+    }
+
+    public static void runExternalCommand(String command, String[] args) {
+        try {
+            String path = getPath(command);
+            if (path == null) {
+                System.out.println(command + ": command not found");
+                return;
+            }
+
+            List<String> cmdList = new ArrayList<>();
+            cmdList.add(path);
+            cmdList.addAll(List.of(args));
+
+            ProcessBuilder pb = new ProcessBuilder(cmdList);
+            Process process = pb.start();
+
+            try(BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            }
+            process.waitFor();
+        } catch (Exception e) {
+            System.out.println(command + ": error running command");
         }
     }
 
