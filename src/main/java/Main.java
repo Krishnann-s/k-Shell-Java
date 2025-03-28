ee7f95c2a77c0bfe6e584b4019958eac58898db4
Krishnan <krishnan.spu@gmail.com>
2025-03-29 00:42:12 +0530

    feat: add exit command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
 //         Uncomment this block to pass the first stage
-        Scanner scanner = new Scanner(System.in);
         while(true) {
             System.out.print("$ ");
+
+            Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+
+            if(input.equals("exit 0")) {
+                System.exit(0);
+            }
+
             System.out.println(input + ": command not found");
         }
     }
